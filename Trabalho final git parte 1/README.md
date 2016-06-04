e6eccd39c520ce32d721416930dd5ed03786582d
jhonatan <jhonatan_saman@hotmail.com>
2016-06-04 15:49:48 -0300

    Revert "trabalho"
    This reverts commit 491cc7f5952ecf7d4531547e4cb91164cdf6ef4d.

--- Trabalho final git parte 1/README.md
+++
@@ -1,1 +0,0 @@
-"# jhonatansaman..github.io" 
--- Trabalho final git parte 1/js/formulario.js
+++
@@ -1,9 +0,0 @@
-function sendMail() {
-    var link = "mailto:jhonatan.samann@gmail.com"
-             + "?cc=jhonatan_saman@gmail.com"
-             + "&subject=" + escape("This is my subject")
-             + "&body=" + escape(document.getElementById('myText').value)
-    ;
-
-    window.location.href = link;
-}
